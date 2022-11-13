2ce889448d32e985a2bd2a560ebba611e8731045
Matthew Wybranski <mattrw7104@gmail.com>
2022-11-13 18:43:15 -0500

    Custom Behavior: Green and Red balls both increase size upon collision

---
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject greenBallPrefab;
+    public GameObject plane;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        if (OtherBall.GetComponent<RedBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Red");
+
+            transform.localScale += new Vector3 (1,1,1);
+        }
+    }
+}
+
---
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        if (OtherBall.GetComponent<GreenBallCollisionManager>() != null)
+        {
+            Debug.Log("Red Collided With Green");
+
+            transform.localScale += new Vector3(1, 1, 1); 
+
+        }
+    }
+}
+
